436fd3b682c1966e77bf4b8f0b400ef3401463f9
Tensael <70529266+Tensael@users.noreply.github.com>
2020-09-23 09:35:14 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var deHamburger = document.querySelector("h2");
+
+function doeFormHeenEnWeer(){
+	let hetMenu = document.querySelector("nav");
+	hetMenu.classList.toggle("toonMenu");
+}
+
+deHamburger.addEventListener("click", doeFormHeenEnWeer);
